d61f22361d242afb30391b879c0efd5605a85a19
Mario Fernandes <marioromulof@gmail.com>
2025-10-20 09:37:44 -0300

    feat(zag): configured Redis connection with okredis

--- bitonic-sequence/src/main.zig
+++ bitonic-sequence/src/main.zig
@@ -1,8 +1,49 @@
 const std = @import("std");
 const zap = @import("zap");
-const bitonic = @import("bitonic-array.zig");
+const print = std.debug.print;
+const okredis = @import("okredis");
+
+var redisClient: ?okredis.Client = null;
+var redisRbuf: [1024]u8 = undefined;
+var redisWbuf: [1024]u8 = undefined;
 
 pub fn main() !void {
+    const net = std.net;
+    print("Connecting to Redis at redis:{}\n", .{6379});
+
+    redisClient = blk: {
+        print("Connecting to redis service...\n", .{});
+
+        var addressList = net.getAddressList(std.heap.page_allocator, "localhost", 6379) catch |err| {
+            print("Could not resolve redis service: {}\n", .{err});
+            break :blk null;
+        };
+        defer addressList.deinit();
+
+        if (addressList.addrs.len == 0) {
+            print("No address found for redis service\n", .{});
+            break :blk null;
+        }
+
+        const redisAddr = addressList.addrs[0];
+
+        const connection = net.tcpConnectToAddress(redisAddr) catch |err| {
+            print("Could not connect to Redis: {}\n", .{err});
+            break :blk null;
+        };
+
+        const client = okredis.Client.init(connection, .{
+            .reader_buffer = &redisRbuf,
+            .writer_buffer = &redisWbuf,
+        }) catch |err| {
+            print("Could not initialize Redis client: {}\n", .{err});
+            break :blk null;
+        };
+
+        print("Successfully conected to Redis!\n", .{});
+        break :blk client;
+    };
+
     var listener = zap.HttpListener.init(.{
         .port = 8080,
         .on_request = handleRequest,
@@ -26,6 +67,7 @@ fn handleRequest(r: zap.Request) !void {
 }
 
 fn handleBitonic(r: *const zap.Request) !void {
+    const bitonic = @import("bitonic-array.zig");
     const allocator = std.heap.page_allocator;
 
     const body = r.body orelse {
